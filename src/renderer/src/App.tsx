13dfa2bcbea9ce21acd169bd6f06273ade85965e
LuanRaasch <luanraasch@outlook.com>
2025-08-20 10:06:22 -0300

    feat: criado a pagina home

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,12 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Routes } from './Routes'
+import { queryClient } from './lib/react-query'
 
 export default function App() {
   return (
-    <Routes />
+    <QueryClientProvider client={queryClient}>
+      <Routes />
+    </QueryClientProvider>
+
   )
 }
--- src/renderer/src/pages/home.tsx
+++ src/renderer/src/pages/home.tsx
@@ -1,33 +1,60 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Link } from 'react-router-dom'
 
 export function Home() {
-  async function handleAdd() {
-    const response = await window.api.fetchAllCustomers()
-    console.log(response)
-  }
+  const queryClient = useQueryClient();
 
-  async function handleCustomerById() {
-    const docId = '1938df70-a2ec-4993-818b-67e735a88b1f'
-    const response = await window.api.fetchCustomerById(docId)
-    console.log(response)
-  }
+  // Buscar os clientes
+  const { data } = useQuery({
+    queryKey: ["customers"], queryFn: async () => {
+      const response = await window.api.fetchAllCustomers();
+      console.log(response);
+      return response;
+    }
+  })
 
-  async function handleDeleteCustomer() {
-    const docId = '1938df70-a2ec-4993-818b-67e735a88b1f'
-    const response = await window.api.deleteCustomer(docId)
-    console.log(response)
-  }
+  // async function handleAdd() {
+  //   const response = await window.api.fetchAllCustomers()
+  //   console.log(response)
+  // }
+
+  // async function handleCustomerById() {
+  //   const docId = '1938df70-a2ec-4993-818b-67e735a88b1f'
+  //   const response = await window.api.fetchCustomerById(docId)
+  //   console.log(response)
+  // }
+
+  // async function handleDeleteCustomer() {
+  //   const docId = '1938df70-a2ec-4993-818b-67e735a88b1f'
+  //   const response = await window.api.deleteCustomer(docId)
+  //   console.log(response)
+  // }
 
   return (
-    <div>
-      <h1>Página HOME!!</h1>
-      <Link to="/create">Ir para a página create</Link>
-      <br></br>
-      <button onClick={handleAdd}>BUSCAR USUARIOS</button>
-      <br></br>
-      <button onClick={handleCustomerById}>BUSCAR USUARIO PELO ID</button>
-      <br></br>
-      <button onClick={handleDeleteCustomer}>DELETAR CLIENTE</button>
+    <div className='flex-1 flex flex-col py-12 text-white'>
+      <div className='px-10'>
+        <h1 className='text-white text-xl lg:text-2xl font-semibold mb-4'>
+          Todos clientes
+        </h1>
+      </div>
+
+      <section className='flex flex-col gap-6 w-full h-screen overflow-y-auto px-10 pb-[200px]'>
+        {data?.map((customer) => (
+          <Link
+            to="/"
+            key={customer._id}
+            className='bg-gray-800 px-4 py-3 rounded'
+          >
+            <p className='mb-2 font-semibold text-lg'>{customer.name}</p>
+            <p><span className='font-semibold'>Email: </span>{customer.email}</p>
+            {customer.phone && (
+              <p>
+                <span className='font-semibold'>Telefone: </span> {customer.phone}
+              </p>
+            )}
+          </Link>
+        ))}
+      </section>
     </div>
   )
 }
